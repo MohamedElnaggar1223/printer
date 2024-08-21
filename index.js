249c58097ef1771a69accde713e6f72a988102d4
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-08-21 13:34:36 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-core')
 const chromium = require('chrome-aws-lambda')
 const { execFile } = require('child_process')
 const ptp = require('pdf-to-printer');
